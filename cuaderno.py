bfa4b622ca4ad49f6ca75b0b4608438e9d1e7e50
agustinldg <agustinldg@gmail.com>
2024-10-17 11:36:05 +0200

    arregla chapuza

--- cuaderno.py
+++ cuaderno.py
@@ -9,20 +9,19 @@ from crea_hoja import crea_hoja
 
 
 
-Nombre_de_la_hoja_Excel="4a_Excel_test"
-Nombre_de_la_hoja_Excel="4a_Excel_test"
+Nombre_de_la_hoja_Excel="5a_Excel_test"
 Lista_de_operaciones="  42:3 123+4 279992*1 327:1 12+23+45+4344 12:3 12345:6"
 
 Tamano_tipo_de_letra=26  # font size 11
 
 Color_Respuesta_Final_Correcta='2EFE2E'    #2EFE2E darkgreen (el de la primera versin)
 
-Color_Respuesta_Intermedia_Correcta='FF77FF'    #E0F8E6 green (el de la lprimera version)
+Color_Respuesta_Intermedia_Correcta='B3648F'    #E0F8E6 green (el de la lprimera version)
  
 
 
 Modos_Disponibles="Fácil,Pro"
-Modos_Disponibles="Fácil,Pro"
+
 
 Modo_Seleccionado="SuperPro"
 
@@ -47,8 +46,6 @@ builtins.CFG_MODO_SELEC=Modo_Seleccionado
 builtins.CFG_COLOR_FINAL_OK=Color_Respuesta_Final_Correcta
 builtins.CFG_COLOR_INTERMEDIO_OK=Color_Respuesta_Intermedia_Correcta
 
-builtins.CFG_COLOR_FINAL_OK=Color_Respuesta_Final_Correcta
-builtins.CFG_COLOR_INTERMEDIO_OK=Color_Respuesta_Intermedia_Correcta
 
 builtins.CFG_OPERACIONES_POR_FILA=4
 builtins.CFG_CELDAS_POR_OPERACION=12
